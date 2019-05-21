e4321f6ee10b5dbadf486e3fa37683de768a44f7
Rylan Sturm <sturm.rw@gmail.com>
2019-05-20 22:09:39 -0600

    schedule api added

--- app/api/schedules.py
+++ app/api/schedules.py
@@ -3,7 +3,6 @@ from app.models import Schedule, Area, Shift
 from app.api import bp
 from app import db
 from functions.dates import datetime_from_string as dfs
-import datetime
 
 
 @bp.route('/schedules/config', methods=['POST'])
@@ -13,19 +12,12 @@ def create_schedule():
     data['schedule_shift'] = Shift.query.filter_by(name=data['schedule_shift'] or None).first()
     for time in ['start1', 'start2', 'start3', 'start4', 'end1', 'end2', 'end3', 'end4']:
         data[time] = dfs(data[time])
-    if data['name'] == 'Regular':
-        schedule = Schedule.query.filter_by(schedule_area=data['schedule_area'], schedule_shift=data['schedule_shift'],
-                                            name=data['name']).first()
-    else:
-        schedule = Schedule.query.filter_by(schedule_area=data['schedule_area'], schedule_shift=data['schedule_shift'],
-                                            start1=data['start1'], start2=data['start2'], start3=data['start3'],
-                                            start4=data['start4'], end1=data['end1'], end2=data['end2'],
-                                            end3=data['end3'], end4=data['end4']).first()
+    schedule = Schedule.query.filter_by(schedule_area=data['schedule_area'], schedule_shift=data['schedule_shift'],
+                                        name=data['name']).first()
     if schedule:
         s = schedule
     else:
         s = Schedule()
-        s.name = 'Custom {}'.format(str(datetime.date.today()))
     s.from_dict(data)
     db.session.add(s)
     db.session.commit()
